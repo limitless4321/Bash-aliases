7fe5749e6492f564872cf6e34810ef0db3fcebd5
limitless4321 <123585667+limitless4321@users.noreply.github.com>
2026-05-23 11:41:38 +0100

    Add RUN_FIRST.sh for setup (run once and first, then commands will work)

---
+++ RUN_FIRST.sh
@@ -0,0 +1,18 @@
+# Create /cheese directory if missing
+sudo mkdir -p /cheese
+
+# Create cheese.img if missing
+if [ ! -f /cheese.img ]; then
+    # Make a 5MB empty file
+    sudo dd if=/dev/zero of=/cheese.img bs=1M count=5
+
+    # Format it as FAT16
+    sudo mkfs.vfat -F 16 /cheese.img
+fi
+
+sudo mkdir -p /keyboardmapthing
+
+if [ ! -f /keyboardmapthing/us-gb.txt ]; then
+    echo "us" | sudo tee /keyboardmapthing/us-gb.txt > /dev/null
+fi
+
